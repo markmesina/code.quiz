875f7b89bc27cf6271967deafcbd6c0101f302b5
ceress <markmesina69@gmail.com>
2020-01-31 08:40:32 -0800

    entered array of questions

--- script.js
+++ script.js
@@ -26,6 +26,27 @@ function setTime(){
 setTime();
 
 //   on start button click:
+$startQuiz.addEventListener('click', function(){
+  let questions = [
+    {
+      title: "Which of the following is NOT a javascript data type?",
+      choices: ["Array", "Object", "Table", "String"],
+      answer: "Table"
+    },
+    {
+      title: "Which parenthesis type is used to invoke functions?",
+      choices: ["{}", "[]", "||", "()"],
+      answer: "()"
+    },
+    {
+      title: "What will this statement return: console.log('7' == 7)",
+      choices: ["true", "false", "maybe", "depends"],
+      answer: "true"
+    }];
+    questions.forEach(element => { 
+      
+    });
+});
 
 
 //     hide game-start div 
