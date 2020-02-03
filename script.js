1f533597a75918d734d93728e4fffb1dc6a68c3c
ceress <markmesina69@gmail.com>
2020-02-03 13:10:45 -0800

    time penalty for wrong answer

--- script.js
+++ script.js
@@ -3,11 +3,26 @@
 // show game start screen
 // include how to play instructions
 
-// var welcome = alert('Welcome to Code Quiz! Answer select questions and compare scores with fellow Developers!');
-// var howToPlay = alert ('Select correct answer and hit next question button');
-// var beware = alert ('Beware! If you make a mistake, you lose 5 seconds!');
-// var haveFun = alert ('HAVE FUN!');
+var welcome = alert('Welcome to Code Quiz! Answer select questions and compare scores with fellow Developers!');
+var howToPlay = alert ('Select correct answer and hit next question button');
+var beware = alert ('Beware! If you make a mistake, you lose 5 seconds!');
+var haveFun = alert ('HAVE FUN!');
+const $timer = document.querySelector('.timer');
+var timeLeft = 60;
 
+function setTime(){
+  let timeInterval = setInterval(function(){
+    timeLeft--;
+    $timer.textContent = `Time: ${timeLeft} seconds remaining`;
+
+    if(timeLeft === 0) {
+      clearInterval(timeInterval);
+      alert('thank you for playing!');
+    }
+  }, 1000);
+}
+//     start timer
+setTime();
 //     hide game-start div 
 //     show questions div
 //     show question
@@ -24,7 +39,12 @@ Quiz.prototype.getQuestionIndex = function() {
 Quiz.prototype.guess = function(answer) {
   if(this.getQuestionIndex().isCorrectAnswer(answer)) {
       this.score++;
-  }
+      //time penalty
+      timeLeft += 5;
+	} else {
+		timeLeft -= 5;
+	}
+  
 
   this.questionIndex++;
 }
@@ -40,37 +60,53 @@ function Question(text, choices, answer) {
   this.answer = answer;
 }
 
-function Quiz(questions) {
-  this.score = 0;
-  this.questions = questions;
-  this.questionIndex = 0;
+Question.prototype.isCorrectAnswer = function(choice) {
+  return this.answer === choice;
 }
 
-Quiz.prototype.getQuestionIndex = function() {
-  return this.questions[this.questionIndex];
-}
 
-//     if correct:
-//     check to see if answer is correct
-Quiz.prototype.guess = function(answer) {
-  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-    //     add to score
-      this.score++;
+function populate() {
+  if(quiz.isEnded()) {
+      showScores();
   }
+  else {
+      // show question
+      var element = document.getElementById("question");
+      element.innerHTML = quiz.getQuestionIndex().text;
 
-  this.questionIndex++;
-}
+      // show options
+      var choices = quiz.getQuestionIndex().choices;
+      for(var i = 0; i < choices.length; i++) {
+          var element = document.getElementById("choice" + i);
+          element.innerHTML = choices[i];
+          guess("btn" + i, choices[i]);
+      }
 
-Quiz.prototype.isEnded = function() {
-  return this.questionIndex === this.questions.length;
-}
+      showProgress();
+  }
+};
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function() {
+      quiz.guess(guess);
+      populate();
+  }
+};
 
 
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
-}
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+  var gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHTML;
+};
 
 // create questions here
 var questions = [
