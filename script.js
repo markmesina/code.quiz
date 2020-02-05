eafb4e0a5f1e414916cda12516f833c984ac4f89
ceress <markmesina69@gmail.com>
2020-02-04 20:14:51 -0800

    final tweak

--- script.js
+++ script.js
@@ -1,158 +1,222 @@
-
-// on page load: 
-// show game start screen
-// include how to play instructions
-
-const welcome = alert('Welcome to Code Quiz! Answer select questions and compare scores with fellow Developers!');
-const howToPlay = alert ('Select correct answer and hit next question button');
-const beware = alert ('Beware! If you make a mistake, you lose 5 seconds!');
-const haveFun = alert ('HAVE FUN!');
-const startQuizBtn = document.querySelector("#startQuiz");
-const $timer = document.querySelector('.timer');
-let timeLeft = 60;
-
-startQuizBtn.addEventListener("click", quizStart);
-function quizStart() {
-  setTime();
-  populate();
-};
-
-function setTime(){
-  let timeInterval = setInterval(function(){
-    timeLeft--;
-    $timer.textContent = `Time: ${timeLeft} seconds remaining`;
-
-    if(timeLeft === 0) {
-      clearInterval(timeInterval);
-      alert('thank you for playing!');
-    }
-  }, 1000);
+// Data to be used
+var state = {
+  currentQuestion: 0,
+  remainingTime: questions.length * 15, // from questions.js file
+  timer: 0,
+  timeOut: 0,
+  timePenalty: 15,
+  highScores:[] // [{initials: , score: }]
 }
 
-//     hide game-start div 
-//     show questions div
-//     show question
-function Quiz(questions) {
-  this.score = 0;
-  this.questions = questions;
-  this.questionIndex = 0;
-}
 
-Quiz.prototype.getQuestionIndex = function() {
-  return this.questions[this.questionIndex];
-}
+// Initiating func
+function init(){
 
-Quiz.prototype.guess = function(answer) {
-  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-      this.score++;
-      //time penalty
-      timeLeft += 5;
-	} else {
-		timeLeft -= 5;
-	}
+  // Get data from localStorage
+  var fromLocal = localStorage.getItem('highScores');
+      if(fromLocal){ state.highScores = JSON.parse(fromLocal); }
+
+  // Initiate page
+  document.querySelector('#intro').setAttribute('style','display:block;');
+  document.querySelector('#scores').setAttribute('style','display:none;');
+
+  // Initiate data
+  state.currentQuestion = 0;
+  state.remainingTime = questions.length * 15;
+  state.timer = 0;
+  state.timeOut = 0;
+  document.querySelector('#time__num').innerText = state.remainingTime;
+}
+// Questions page(get one key-value set of question as a param)
+function quizRender(data){
+
+  document.querySelector('#quiz').setAttribute('style','display:block;');
+
+// Render heading
+  var question = "<h2>" + data.title + "</h2>"
+      document.querySelector('.quiz__title').innerHTML= question;
+
+// Render buttons
+  // 1. Delete previous question's btns
+  var choicesDOM = document.querySelector('.quiz__choices');
+      deleteChild(choicesDOM); // Func deleting all children
+
+  // 2. Create btns
+  for(var i=0; i < data.choices.length; i++){
+
+      var choice = document.createElement('button');
+          choice.innerText = (i+1) + '. ' + data.choices[i];
+          choice.classList.add("choiceBtn", "btn", "btn-success" ); // classes for style, bootstrap
+
+      // Set data attr to check the correct answer later
+      var answer = ( data.answer === data.choices[i] ) ? "correct" : "wrong";
+          choice.setAttribute('data-answer', answer);
+
+      choicesDOM.appendChild(choice);
+  }
+}
+// Timer & Timeout
+function timerFunc(){
   
+  // Func clearing timer and timeout that previously executed and currently running
+  clearTime();
 
-  this.questionIndex++;
+  var timeDOM = document.querySelector('#time__num');
+      timeDOM.innerText = state.remainingTime; 
+  
+  state.timer = setInterval(function(){
+                  state.remainingTime --;
+                  timeDOM.innerText = state.remainingTime; // countdown
+              },1000)
+
+  state.timeOut = setTimeout(function(){
+                      clearInterval(state.timer);
+                      result(); // func rendering result page
+                  }, state.remainingTime * 1000)
 }
+// Result page
+function result(){
+  
+  document.querySelector('#quiz').setAttribute('style','display:none;');
+  document.querySelector('#result').setAttribute('style','display:block;');
+  document.querySelector('.result__score').innerText = state.remainingTime;
 
-Quiz.prototype.isEnded = function() {
-  return this.questionIndex === this.questions.length;
+  clearInterval(state.timer);
 }
+// Render highscores page
+function renderHighScores(){
 
+  // 1. Sort scores(high score -> low score)
+  state.highScores.sort( function(a,b){return b.score - a.score} );
 
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
+  // 2. Delete all remaining scores that previously printed in page
+  var scoresDOM = document.querySelector('#scores__ranking');
+                  deleteChild(scoresDOM);
+  
+  // 3. Newly render
+  state.highScores.forEach( function(el,i){
+
+      var rank = document.createElement('p')
+          rank.innerText = (i+1) + ". " + el.initials + " - " + el.score;
+          
+          scoresDOM.appendChild(rank);
+
+  });
 }
+// Render 'right' or 'correct' notification(get a param for a word saved as data attr in each choice DOM)
+function verdict(word){
 
-Question.prototype.isCorrectAnswer = function(choice) {
-  return this.answer === choice;
+  document.querySelector('#verdict__word').innerText = word;
+  document.querySelector('#verdict').setAttribute('style','opacity:1;');
+
+  setTimeout(function(){
+      document.querySelector('#verdict').setAttribute('style','opacity:0;');
+  },1000);
 }
-
-
-function populate() {
-  if(quiz.isEnded()) {
-      showScores();
+// Utility - delete children
+function deleteChild(DOM){
+  if(DOM.children){
+          Array.from( DOM.children ).forEach( function(el){ el.remove(); } );
   }
-  else {
-      // show question
-      var element = document.getElementById("question");
-      element.innerHTML = quiz.getQuestionIndex().text;
+}
+// Utility - clear timer and timeout
+function clearTime(){
+  if(state.timer > 0) { clearInterval(state.timer); }
+  if(state.timeOut > 0) { clearTimeout(state.timeOut); }
+}
 
-      // show options
-      var choices = quiz.getQuestionIndex().choices;
-      for(var i = 0; i < choices.length; i++) {
-          var element = document.getElementById("choice" + i);
-          element.innerHTML = choices[i];
-          guess("btn" + i, choices[i]);
-      }
+/****************************
+ BUTTONS & EVENT HANDLER
+*****************************/
 
-      showProgress();
+// Start quiz button
+document.querySelector('#startBtn').addEventListener('click',function(e){
+  // 1. Hide intro section
+  document.querySelector('#intro').setAttribute('style', 'display: none;')
+  // 2. Quiz render
+  quizRender(questions[state.currentQuestion]);
+  // 3. Start timer
+  timerFunc();
+});
+// Question's each choice btn
+document.querySelector('.quiz__choices').addEventListener('click',function(e){
+  
+  // 1. Check if answer is wrong
+  if( e.target.getAttribute('data-answer') !== "correct" ){
+
+      state.remainingTime -= state.timePenalty;
+      timerFunc();
+      verdict("Wrong!");
   }
-};
-
-function guess(id, guess) {
-  var button = document.getElementById(id);
-  button.onclick = function() {
-      quiz.guess(guess);
-      populate();
+  else{
+      verdict("Correct!");
   }
-};
 
+  // 2. Move to next question
+  state.currentQuestion ++;
 
-function showProgress() {
-  var currentQuestionNumber = quiz.questionIndex + 1;
-  var element = document.getElementById("progress");
-  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-};
+  // 3. When there is remaining question
+  if(state.currentQuestion < questions.length){
+      quizRender(questions[state.currentQuestion]);
+  }
+  else{
+      result();
+      clearTimeout(state.timeOut);
+  }
+  
+})
+// Submit btn 
+document.querySelector('#submitBtn').addEventListener('click', function(){
 
-function showScores() {
-  var gameOverHTML = "<h1>Result</h1>";
-  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-  var element = document.getElementById("quiz");
-  element.innerHTML = gameOverHTML;
-};
+  document.querySelector('#result').setAttribute('style','display:none;');
+  document.querySelector('#scores').setAttribute('style','display:block;');
 
-// create questions here
-var questions = [
-  new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
-  new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-  new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
-  new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-  new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
-];
+  // 1. Save user's info to state data obj & localStorage
+  var currentScore = {};
+      currentScore.initials = document.querySelector('#initials').value;
+      currentScore.score = state.remainingTime;
+      
+      state.highScores.push(currentScore);
 
-// create quiz
-var quiz = new Quiz(questions);
+      localStorage.setItem('highScores', JSON.stringify(state.highScores));
 
-// display quiz
-// populate();
-//     on answer selection:
-//     display success message
-//     show next question
-//     if incorrect:
-//     display fail message
-//     deduct time from timer
-//       show next question
-//       for game end:
-//     if all the questions have been answered:
-//     display score
-//       prompt to save initials and results
-//       on save:
-//       record results in local storage
-//       if time runs out:
-//       display score
-//       prompt to try again
+  // 2. Render highscores page
+  renderHighScores()
+});
+// Go back & Clear Highscores btns
+document.querySelector('.scores__btn').addEventListener('click',function(e){
 
-// functions:
-//     resetGame()
-//     endGame()
-//     restartGame()
-//     renderQuestion()
-//     checkAnswer()
-//     saveScore()
-//     clearScores()
-//     toggleScores()
-//     */
+  // 1. Go back btn
+  if(e.target.matches('#gobackBtn')){
+      init();
+  }
 
+  // 2. Clear Highscores btn
+  else if(e.target.matches('#clearBtn')){
+
+      // a. Delete state data obj
+      state.highScores = [];
+      
+      // b. Clear rendered score list
+      var scoresDOM = document.querySelector('#scores__ranking');
+                      deleteChild(scoresDOM);
+      
+      // c. Clear localStorage
+      localStorage.removeItem('highScores');
+  }
+});
+// View highscores button
+document.querySelector('#viewScores').addEventListener('click', function(){
+
+  document.querySelector('#result').setAttribute('style','display:none;');
+  document.querySelector('#quiz').setAttribute('style','display:none;');
+  document.querySelector('#intro').setAttribute('style','display:none;');
+  document.querySelector('#scores').setAttribute('style','display:block;');
+  
+  // Clear time when clicked while proceeding quiz
+  clearTime();
+
+  renderHighScores();
+});
+
+init();
